a43770d74b2919cd19620a4b5e4568e44a449130
Veronika Ihnatova <veronika.a.ignatova@gmail.com>
2023-10-01 20:49:33 +0300

    Add project files.

---
+++ Work/Program.cs
@@ -0,0 +1,33 @@
+﻿static int IntParse(string s)
+{
+    char[] chars = s.ToCharArray();
+    int result = 0;
+
+    for (int i = 0; i < chars.Length; i++)
+    {
+        if (chars[i] >= 48 && chars[i] <= 57)
+        {
+            //Console.WriteLine((int)chars[i]);
+            result *= 10;
+            result += chars[i] - 48;
+        }
+        else
+        {
+            throw new Exception($"{chars[i]} isn`t a number");
+        }
+    }
+    
+    return result;
+}
+
+static string SentenceReverse(string str)
+{
+    return str;
+}
+
+static string[] StringSplit(string sentence, char symbol)
+{
+    return sentence.Split(symbol);
+}
+
+Console.WriteLine(IntParse("150"));
